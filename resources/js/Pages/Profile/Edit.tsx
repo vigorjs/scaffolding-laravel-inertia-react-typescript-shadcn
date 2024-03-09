37646cbc0034a3aed70c11fbc7bce5ef020cb1da
Virgo Fajar Pamungkas <111201912289@mhs.dinus.ac.id>
2024-03-10 02:48:13 +0700

    Update Edit.tsx

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -8,8 +8,7 @@ import { PageProps } from '@/types';
 export default function Edit({ auth, mustVerifyEmail, status }: PageProps<{ mustVerifyEmail: boolean, status?: string }>) {
     return (
         <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Profile</h2>}
+            title='Dashboard'
         >
             <Head title="Profile" />
 
