650001358e44442e272bfbbdda15c08cadf25ed6
Virgo Fajar Pamungkas <111201912289@mhs.dinus.ac.id>
2024-03-10 07:16:55 +0700

    chore: fix spatie-roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        //share data role
+        Inertia::share('role', function () {
+            // Periksa apakah pengguna telah diautentikasi
+            if (Auth::check()) {
+                // Ambil dan kembalikan data role pengguna jika tersedia
+                return Auth::user()->getRoleNames()->first();
+            }
+            return null; // Mengembalikan null jika pengguna tidak diautentikasi
+        });
     }
 
     /**
